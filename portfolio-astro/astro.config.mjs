f9791eee9a6cb625bf17d7c60159e3d85cefa367
carolreds85 <carolreds85@gmail.com>
2026-04-20 18:24:48 -0300

    more fixes

--- portfolio-astro/astro.config.mjs
+++ portfolio-astro/astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: 'https://github.io',
-  base: '/', 
+  base: './portfolio-astro', 
   output: 'static',
   integrations: [sitemap()],
   vite: {
